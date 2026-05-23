6c234014ed2312c17386e0bea0677fd802d23604
Rohit Pasale <rohitpasale1@gmail.com>
2026-05-23 10:52:44 +0530

    js

---
+++ js/baisc.js
@@ -0,0 +1,19 @@
+console.log("hello world");
+// varaibles are container that store data 
+// identifiers are name of variable 
+// in js threee data types 
+// const,var,let => const is if not assign again to variable ,var is functional scope declare in if then use in any place let is block scope 
+const a = 100 ; // can't assign letter
+console.log(a);
+if(true)
+{
+    var  x = 1000;
+    let y = 200;
+}
+console.log(x);
+//console.log(y);
+var x1 = 100 ;
+var x1 = 200;
+console.log(x1);
+// redeclare and functional scope of var so that why we use let.. let is block scope and cant declare 
+
