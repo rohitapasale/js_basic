efc00e3dbeff39bf887fba512c14c0ea678cf66a
Rohit Pasale <rohitpasale1@gmail.com>
2026-05-23 11:21:20 +0530

    done

---
+++ js/datatypes.js
@@ -0,0 +1,19 @@
+console.log("datatypes=> in js")
+// 8 data types int,big-int,string,boolean,undefined,null,symbol,object => array is in object
+// num are works on decimal we you to use floor ceil functions
+let num1 = 100,num2 = 30 ;
+console.log(num1/num2);// decimal output 
+console.log(Math.floor(num1/num2));
+console.log(Math.ceil(num1/num2));
+console.log(Math.round(num1/num2));
+console.log((num1/num2).toFixed(3));
+// use this method in operation in operations take care here all decimal are in same varible
+// var can change data type also 
+// undefined data type means varible that not assign yet 
+let x;
+console.log(typeof(x));
+console.log(x);
+// null means give empty val as like in c++
+let z = null ;
+console.log(typeof(z)); //  ** type of null is object imp catch **
+console.log(z);
